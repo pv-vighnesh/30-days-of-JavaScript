08d19a09c7bd67ef3f571fadfc0c4db53aae2b5e
PV Vighnesh <pvighnesh.vishnu@gmail.com>
2021-01-08 21:06:16 +0530

    Datatypes, Inline, internal and External scripts

---
+++ D1/datatype.js
@@ -0,0 +1,6 @@
+console.log(typeof 'hello');
+console.log(typeof 55);
+console.log(typeof 68.89);
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof undefined);
---
+++ D1/intro.js
@@ -0,0 +1,1 @@
+console.log('This is externally connected script file');
